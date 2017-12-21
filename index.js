1decd62762712031f4684ae990bdc41231e36358
sidd <sidd@chartiq.com>
2017-12-21 13:59:27 -0500

    connection list

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var Api = require('./lib/Api');
 exports.AuthModel = require('./lib/AuthModel');
+exports.AppAuthModel = require('./lib/AppAuthModel');
 exports.FeedModel = require('./lib/FeedModel');
 exports.MessageModel = require('./lib/MessageModel');
+exports.ConnectionModel = require('./lib/ConnectionModel');
 
 exports.create = function(urls)
 {
